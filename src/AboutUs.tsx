f836ad47149d5e0f7e3179204610604900d49fbb
Val <val.samonte@gmail.com>
2024-11-10 11:17:54 +0800

    Use fixed

--- src/AboutUs.tsx
+++ src/AboutUs.tsx
@@ -4,7 +4,7 @@ export function AboutUs() {
   return (
     <div className='flex flex-col bg-gray-900 relative z-10'>
       <div className='grid grid-cols-12 p-10 md:p-20 pt-0'>
-        <div className='col-span-12 sm:col-span-7 flex flex-col gap-5 md:gap-10 h-full justify-center'>
+        <div className='col-span-12 sm:col-span-7 flex flex-col gap-5 md:gap-10 h-full'>
           <h2 className='font-bold text-2xl lg:text-5xl text-center sm:text-left'>
             About <wbr />
             Solana Philippines
